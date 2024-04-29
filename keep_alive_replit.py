1d940a328a1dea984799f699baeadeb5c7acfe4e
knightearner <114371387+knightearner@users.noreply.github.com>
2024-04-29 23:48:03 +0530

    Update keep_alive_replit.py

--- keep_alive_replit.py
+++ keep_alive_replit.py
@@ -1,12 +1,68 @@
-from flask import Flask
+from deta import Deta
+
+key='d0p3jsxc_jAhdkSrj194KPkx9YX8iDRZzZCBKQPfP'
+
+def get_switch():
+    deta = Deta(key)
+    users = deta.Base("switch")
+    fetch_res = users.fetch({"key": "ua1hy6g6qak6"})
+    return fetch_res.items[0]['Switch']
+
+def update_switch_OFF():
+    deta = Deta(key)
+    users = deta.Base("switch")
+    users.update(
+    {
+      'Switch': 'OFF'
+    }, 'ua1hy6g6qak6')
+    
+    fetch_res = users.fetch({"key": "ua1hy6g6qak6"})
+    return fetch_res.items[0]['Switch']
+
+def update_switch_ON():
+    deta = Deta(key)
+    users = deta.Base("switch")
+    users.update(
+    {
+      'Switch': 'ON'
+    }, 'ua1hy6g6qak6')
+    
+    fetch_res = users.fetch({"key": "ua1hy6g6qak6"})
+    return fetch_res.items[0]['Switch']
+
+
+from flask import Flask, render_template, request
+
+# app = Flask(__name__)
+
 from threading import Thread
 
 app = Flask('')
 
+light_status = get_switch()
+
 
 @app.route('/')
-def main():
-  return "Your Bot Is Alive"
+def index():
+    return render_template('index.html', status=light_status)
+
+
+@app.route('/switch_on')
+def switch_on():
+    global light_status
+    light_status = update_switch_ON()
+    return render_template('index.html', status=light_status)
+
+
+@app.route('/switch_off')
+def switch_off():
+    global light_status
+    light_status = update_switch_OFF()
+    return render_template('index.html', status=light_status)
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
 
 def run():
